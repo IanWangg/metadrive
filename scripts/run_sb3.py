59a4d9b4887b269bf8b43af111c1a1c3f7c133ee
Yiran Wang <ywang3@bolei-gpu06.cs.ucla.edu>
2024-05-06 16:31:42 -0700

    fix metadrive run script

--- scripts/run_sb3.py
+++ scripts/run_sb3.py
@@ -6,12 +6,15 @@ import numpy as np
 import stable_baselines3 as sb3
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env import VecEnv, SubprocVecEnv, DummyVecEnv
+from stable_baselines3.common.monitor import Monitor
 
 from stable_baselines3.ppo.ppo import PPO
 
 import wandb
 from wandb.integration.sb3 import WandbCallback
 from stable_baselines3.common.callbacks import CallbackList, CheckpointCallback, EvalCallback
+
+from functools import partial
 
 
 # stop logging
@@ -23,28 +26,28 @@ logger.propagate = False
 config = dict(
     env = dict(
         use_render=False,
-        start_seed=0, # must align with the seed in the argument of make_vec_env
         num_scenarios=1000,
         horizon=1000,
         traffic_density=0.1,
         random_traffic=False,
+        accident_prob=0.0,
     ),
     algo = dict(
-        learning_rate=3e-4,
-        n_steps=128,
+        learning_rate=5e-5,
+        n_steps=200,
         batch_size=256,
         n_epochs=10,
         vf_coef=1.0,
-        max_grad_norm=1.0,
+        max_grad_norm=10.0,
         verbose=1,
         seed=0,
         ent_coef=0.0,
-        tensorboard_log="./metadrive_ppo_tb_logs/",
+        tensorboard_log="./metadrive_ppo-single_scenario_per_process_1e8-tb_logs/",
     ),
     n_envs=256,
 )
 
-exptid = "original-metadrive-ppo-may04"
+exptid = "original-metadrive-ppo-single_scenario_per_process_1e8-may04"
 
 # run = wandb.init(
 #         project="metaurban-rl",
@@ -58,27 +61,39 @@ exptid = "original-metadrive-ppo-may04"
 
 env_cfg = config["env"]
 
-def make_metadrive_env_fn():
-    return MetaDriveEnv
+def make_metadrive_env_fn(env_cfg, seed):
+    env = MetaDriveEnv(dict(                     
+                      start_seed=seed,
+                      log_level=50,
+                      **env_cfg,
+                      ))
+    env = Monitor(env)
+    return env
 
 
 def train():
-    env_fn = make_metadrive_env_fn()
-    env = make_vec_env(
-        env_id=env_fn, 
-        vec_env_cls=SubprocVecEnv, # must use SubprocVecEnv, otherwise the error will occur
-        env_kwargs=dict(config=env_cfg),
-        seed=0, # must pass seed to make_vec_env, otherwise the seed will be randomly generated, which will cause the error in metadrive
-        n_envs=config["n_envs"] # seed 0 - 255
-    )
+    env_fn = make_metadrive_env_fn
+    # env = make_vec_env(
+    #     env_id=env_fn, 
+    #     vec_env_cls=SubprocVecEnv, # must use SubprocVecEnv, otherwise the error will occur
+    #     env_kwargs=dict(config=env_cfg),
+    #     seed=0, # must pass seed to make_vec_env, otherwise the seed will be randomly generated, which will cause the error in metadrive
+    #     n_envs=config["n_envs"] # seed 0 - 255
+    # )
 
-    eval_env = make_vec_env(
-        env_id=env_fn, 
-        vec_env_cls=SubprocVecEnv, # must use SubprocVecEnv, otherwise the error will occur
-        env_kwargs=dict(config=env_cfg),
-        seed=250, # must pass seed to make_vec_env, otherwise the seed will be randomly generated, which will cause the error in metadrive
-        n_envs=16 # seed 250 - 255
-    )
+    # eval_env = make_vec_env(
+    #     env_id=env_fn, 
+    #     vec_env_cls=SubprocVecEnv, # must use SubprocVecEnv, otherwise the error will occur
+    #     env_kwargs=dict(config=env_cfg),
+    #     seed=250, # must pass seed to make_vec_env, otherwise the seed will be randomly generated, which will cause the error in metadrive
+    #     n_envs=16 # seed 250 - 255
+    # )
+
+    env_cfg = config["env"]
+
+    env = SubprocVecEnv([partial(env_fn, env_cfg, seed) for seed in range(config["n_envs"])])
+
+    eval_env = SubprocVecEnv([partial(env_fn, env_cfg, seed) for seed in range(config["n_envs"], config["n_envs"]+16)])
 
     model = PPO(
         "MlpPolicy", 
@@ -87,14 +102,14 @@ def train():
     )
 
     checkpoint_callback = CheckpointCallback(
-        save_freq=int(1e5),
+        save_freq=int(1e3),
         save_path="./ckpt_logs/",
         name_prefix=exptid,
         save_vecnormalize=True,
     )
 
     eval_callback = EvalCallback(eval_env, best_model_save_path="./eval_logs/",
-                             log_path="./eval_logs/", eval_freq=int(5e4),
+                             log_path="./eval_logs/", eval_freq=int(1e3),
                              deterministic=True, render=False)
     
     # wandb_callback = WandbCallback()
@@ -103,8 +118,9 @@ def train():
     callbacks = CallbackList([checkpoint_callback, eval_callback])
 
     model.learn(
-        total_timesteps=int(1e7),
+        total_timesteps=int(1e8),
         callback=callbacks,
+        log_interval=4,
     )
     env.close()
 
